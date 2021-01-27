dec18262a110bb29d750f8257feb8753d7dd6e56
Roman <romanyasinskyiif@gmail.com>
2021-01-27 16:55:15 +0200

    createHomeScreenAndProductComponent

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,19 @@
+import Footer from "./components/Footer"
+import Header from "./components/Header"
+import { Container } from "react-bootstrap"
+import HomeScreen from "./screens/HomeScreen"
 
 const App = () => {
   return (
     <>
-      <h1>Welcome to BitLink</h1>
-    </>
+      <Header />
+      <main className='py-3'>
+        <Container>
+         <HomeScreen />
+        </Container>
+      </main>
+      <Footer />  
+      </>
   );
 }
 
