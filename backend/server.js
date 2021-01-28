9ba7868f0f881273587f5c32fd2d308765be56aa
Roman <romanyasinskyiif@gmail.com>
2021-01-28 12:00:07 +0200

    FetchProducts

--- backend/server.js
+++ backend/server.js
@@ -4,11 +4,11 @@ const products = require('./data/products');
 const app = express();
 
 app.get('/', (req, res)=>{
-    res.send('API is running')
+    res.send('API is running');
 });
 
 app.get('/api/products', (req, res)=>{
-    res.json(products)
+    res.json(products);
 });
 
 app.get('/api/products/:id', (req, res)=>{
